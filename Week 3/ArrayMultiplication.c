7ed9b9b0c306a672ef57d1898a9be0026e970255
Param Jayesh Kansagra <paramkansagra@gmail.com>
2022-03-02 17:17:56 +0530

    Added Descripton of the problem

--- Week 3/ArrayMultiplication.c
+++ Week 3/ArrayMultiplication.c
@@ -1,3 +1,41 @@
+// Description
+
+// Given the size and array elements, write a program in C to update every array element
+// with the multiplication of previous and next numbers in array
+
+// Print ‘invalid’, if the boundary conditions are not met.
+
+// For example, Input
+// 6
+// 1 2 3 4 5 6
+
+// Output
+
+// 2 3 8 15 24 30
+
+// Explanation:
+
+
+// Perform element-wise operation,
+// Multiply Previous no with next no.2(1*2)3(1*3)8(2*4)15(3*5)24(4*6)30(5*6)
+
+// Boundary Conditions:
+
+// n >= 1
+
+// element > 0
+
+// Input Format
+
+// First-line contains the size of the array.
+// The next line contains, space-separated, the elements of the array
+
+// Output Format
+
+// Print the array in such a way that, it updates every array element with the multiplication
+// of previous and next numbers in the given array
+
+
 #include <stdio.h>
 
 int main(){
